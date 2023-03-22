594978456f955e9881854cb4f7994de615829a79
FedeDiaz31 <fede.diaz1231@gmail.com>
2023-03-22 15:24:51 -0300

    User Routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,32 +1,47 @@
 const { User } = require("../models");
 
 // Display a listing of the resource.
-async function index(req, res) {}
+async function index(req, res) {
+  const users = await User.find();
+  res.json(users);
+}
 
 // Display the specified resource.
-async function show(req, res) {}
+async function show(req, res) {
+  const userId = req.params.id;
+  const user = await User.findById(userId);
+  res.json(user);
+}
 
 // Show the form for creating a new resource
 async function create(req, res) {
   const bodyData = req.body;
-    const newUser = await User.create({
+  const newUser = await User.create({
+    firstname: bodyData.firstname,
+    lastname: bodyData.lastname,
+    password: bodyData.password,
+    email: bodyData.email,
+    address: bodyData.address,
+  });
+  res.json(newUser);
+}
+
+// Show the form for editing the specified resource.
+async function edit(req, res) {
+  const bodyData = req.body;
+  const userId = req.params.id;
+  const user = await User.findByIdAndUpdate(
+    { _id: userId },
+    {
       firstname: bodyData.firstname,
       lastname: bodyData.lastname,
       password: bodyData.password,
       email: bodyData.email,
       address: bodyData.address,
-    });
-    res.json(newUser);
+    }
+  );
+  res.json(user);
 }
-
-// Store a newly created resource in storage.
-async function store(req, res) {}
-
-// Show the form for editing the specified resource.
-async function edit(req, res) {}
-
-// Update the specified resource in storage.
-async function update(req, res) {}
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {
@@ -39,8 +54,6 @@ module.exports = {
   index,
   show,
   create,
-  store,
   edit,
-  update,
   destroy,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,8 @@ const userController = require("../controllers/userController");
 
 router.get("/", userController.index);
 router.post("/create", userController.create);
-router.post("/", userController.store);
 router.get("/:id", userController.show);
 router.patch("/:id/edit", userController.edit);
-router.patch("/:id", userController.update);
 router.delete("/:id", userController.destroy);
 
 module.exports = router;

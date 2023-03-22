298da4a02eccff87fb55a6f6b542c9dbff83c9ac
FedeDiaz31 <fede.diaz1231@gmail.com>
2023-03-22 12:55:16 -0300

    Create Product - Backend

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,12 +9,29 @@ async function index(req, res) {
 // Display the specified resource.
 async function show(req, res) {
   const productId = req.params.id;
-  const product = await Product.findById(productId)
-  res.json(product)
+  const product = await Product.findById(productId);
+  res.json(product);
 }
 
 // Show the form for creating a new resource
-async function create(req, res) {}
+async function create(req, res) {
+  function createSlug(model) {
+    const slug = model.split(" ").join("-").toLowerCase();
+
+    return slug;
+  }
+  console.log(createSlug);
+  const newProduct = await Product.create({
+    brand: req.body.brand,
+    model: req.body.model,
+    slug: createSlug(req.body.model),
+    image: [req.body.image],
+    highlight: req.body.highlight,
+    price: req.body.price,
+    stock: req.body.stock,
+  });
+  res.json(newProduct);
+}
 
 // Store a newly created resource in storage.
 async function store(req, res) {}
@@ -27,9 +44,9 @@ async function update(req, res) {}
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {
-    const productId = req.params.id;
-    const product = await Product.findByIdAndDelete(productId)
-    res.json({ message: 'The Product was deleted', productDeleted: product})
+  const productId = req.params.id;
+  const product = await Product.findByIdAndDelete(productId);
+  res.json({ message: "The Product was deleted", productDeleted: product });
 }
 
 module.exports = {

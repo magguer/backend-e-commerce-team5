62aacfed49dcfe853e4211c42f5de11b639a751d
Juan Manuel Ottado <juanmanuelottado@gmail.com>
2023-04-03 01:47:53 -0300

    Revert "server-running-models-v1"
    This reverts commit 57c1224ec108392714ce47503cc7dec060a9f2a2.

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,19 @@
-// bussiness 
-const categoryRoutes = require("./business/categoryRoutes");
-const clientRoutes = require("./business/clientRoutes");
-const discountGroupRoutes = require("./business/discountGroupRoutes");
-const noboxRoutes = require("./business/noboxRoutes");
-const productRoutes = require("./business/productRoutes");
-const serviceRoutes = require("./business/serviceRoutes");
-const subcategoryRoutes = require("./business/subcategoryRoutes");
-
-// payment 
-const billRoutes = require("./payment/billRoutes");
-const bookingRoutes = require("./payment/bookingRoutes");
-const orderRoutes = require("./payment/orderRoutes");
-const statusRoutes = require("./payment/statusRoutes");
-
-// project
-const headingRoutes = require("./project/headingRoutes");
-const projectRoutes = require("./project/projectRoutes");
-const roleProjectRoutes = require("./project/roleProjectRoutes");
-
-// user 
-const addressRoutes = require("./user/addressRoutes");
-const adminRoutes = require("./user/adminRoutes");
-const roleAdminRoutes = require("./user/roleAdminRoutes");
-const userRoutes = require("./user/userRoutes");
-
-
-
+const adminRoutes = require("./adminRoutes");
+const userRoutes = require("./userRoutes");
+const productRoutes = require("./productRoutes");
+const categoryRoutes = require("./categoryRoutes");
+const statusRoutes = require("./statusRoutes");
+const orderRoutes = require("./orderRoutes");
+const brandRoutes = require("./brandRoutes");
+const billRoutes = require("./billRoutes");
 
 module.exports = (app) => {
-  app.use("/category", categoryRoutes);
-  app.use("/client", clientRoutes);
-  app.use("/discountGroup", discountGroupRoutes);
-  app.use("/nobox", noboxRoutes);
-  app.use("/product", productRoutes);
-  app.use("/services", serviceRoutes);
-  app.use("/subcategory", subcategoryRoutes);
-
-  app.use("/bill", billRoutes);
-  app.use("/booking", bookingRoutes);
-  app.use("/status", statusRoutes);
-  app.use("/order", orderRoutes);
-
-  app.use("/heading", headingRoutes);
-  app.use("/project", projectRoutes);
-  app.use("/roleProject", roleProjectRoutes);
-
-  app.use("/address", addressRoutes);
+  app.use("/categories", categoryRoutes);
   app.use("/admin", adminRoutes);
-  app.use("/roleAdmin", roleAdminRoutes);
+  app.use("/status", statusRoutes);
+  app.use("/brands", brandRoutes);
+  app.use("/orders", orderRoutes);
+  app.use("/products", productRoutes);
   app.use("/users", userRoutes);
-
+  app.use("/bills", billRoutes);
 };
---
+++ routes/orderRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const { expressjwt: checkJwt } = require("express-jwt");
+const orderController = require("../controllers/orderController");
+
+router.use(
+  checkJwt({
+    secret: process.env.SESSION_SECRET,
+    algorithms: ["HS256"],
+  })
+);
+
+router.get("/", orderController.index);
+router.get("/last", orderController.lastOrders);
+router.get("/:id", orderController.show);
+router.post("/", orderController.store);
+router.patch("/:id/edit", orderController.edit);
+router.delete("/:id", orderController.destroy);
+
+module.exports = router;

7ebfc0cb1346c33b3effb28b6ad5dd3c031cc50f
Juan Manuel Ottado <juanmanuelottado@gmail.com>
2023-04-03 01:47:48 -0300

    Revert "all-vinculed"
    This reverts commit 8e5264bf92b86a8159bbb435bad5b6b0aa2385a2.

--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 async function runAllSeeders() {
   const { mongoose } = require("../db");
-
   // await mongoose.connection.dropCollection("admins");
   // await mongoose.connection.dropCollection("brands");
   // await mongoose.connection.dropCollection("users");
@@ -11,33 +10,16 @@ async function runAllSeeders() {
   // await mongoose.connection.dropCollection("orders");
   // await mongoose.connection.dropCollection("bills");
 
-  await require("./business/categorySeeder")();
-  await require("./business/clientSeeder")();
-  await require("./business/discountGroupSeeder")();
-  await require("./business/noboxSeeder")();
-  await require("./business/productSeeder")();
-  await require("./business/serviceSeeder")();
-  await require("./business/subcategorySeeder")();
-
-
-  await require("./payment/billSeeder")();
-  await require("./payment/bookingSeeder")();
-  await require("./payment/orderSeeder")();
-  await require("./payment/statusSeeder")();
-
-  await require("./project/headingSeeder")();
-  await require("./project/projectSeeder")();
-  await require("./project/roleProjectSeeder")();
-
-
-  await require("./user/addressSeeder")();
-  await require("./user/adminSeeder")();
-  await require("./user/roleSeeder")();
-  await require("./user/userSeeder")();
+  await require("./adminSeeder")();
+  await require("./brandSeeder")();
+  await require("./userSeeder")();
+  await require("./categorySeeder")();
+  await require("./productSeeder")();
+  await require("./statusSeeder")();
+  await require("./orderSeeder")();
 
   console.log("[Database] ¡Los datos de prueba fueron insertados!");
   process.exit();
-
 }
 
 runAllSeeders();

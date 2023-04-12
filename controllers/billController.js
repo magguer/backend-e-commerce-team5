cc18eeac8da80fde43206f8c687111cc13d1a8b6
NachoVidal27 <igjovidal@gmail.com>
2023-04-12 17:49:15 -0300

    updated bill controller

--- controllers/billController.js
+++ controllers/billController.js
@@ -14,12 +14,12 @@ async function lastBills(req, res) {
 // Display the specified resource.
 async function show(req, res) {
   const billId = req.params.id;
-  const bill = await Bill.findById(billId);
+  const bill = await Bill.find({ user: req.params.id });
   res.json(bill);
 }
 
 // Show the form for creating a new resource
-async function create(req, res) { }
+async function create(req, res) {}
 
 // Store a newly created resource in storage.
 async function store(req, res) {
@@ -43,13 +43,13 @@ async function store(req, res) {
 }
 
 // Show the form for editing the specified resource.
-async function edit(req, res) { }
+async function edit(req, res) {}
 
 // Update the specified resource in storage.
-async function update(req, res) { }
+async function update(req, res) {}
 
 // Remove the specified resource from storage.
-async function destroy(req, res) { }
+async function destroy(req, res) {}
 
 module.exports = {
   index,
